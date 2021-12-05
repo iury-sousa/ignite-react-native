3249a3e9489cb6a09d6b39fa9037c31fddee3969
Iury Sousa <iurysousa13@outlook.com>
2021-12-05 16:54:06 -0300

    feat: creates components

---
+++ gofinances/src/components/HighlightCard/styles.ts
@@ -0,0 +1,71 @@
+import styled, { css } from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { HighlightCardTypes } from ".";
+
+type TypeProps = {
+  type: HighlightCardTypes;
+};
+
+export const Container = styled.View<TypeProps>`
+  width: ${RFValue(286)}px;
+  padding: 19px 23px;
+  padding-bottom: ${RFValue(42)}px;
+  margin-right: 16px;
+
+  background: ${({ theme, type }) =>
+    type === "total" ? theme.colors.secondary : theme.colors.shape};
+  border-radius: 5px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text<TypeProps>`
+  color: ${({ theme, type }) =>
+    type === "total" ? theme.colors.shape : theme.colors.textDark};
+
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const Icon = styled(Feather)<TypeProps>`
+  font-size: ${RFValue(40)}px;
+
+  ${({ type }) =>
+    type === "up" &&
+    css`
+      color: ${({ theme }) => theme.colors.success};
+    `}
+  ${({ type }) =>
+    type === "down" &&
+    css`
+      color: ${({ theme }) => theme.colors.attention};
+    `}
+  ${({ type }) =>
+    type === "total" &&
+    css`
+      color: ${({ theme }) => theme.colors.shape};
+    `}
+`;
+
+export const Footer = styled.View``;
+
+export const Amount = styled.Text<TypeProps>`
+  margin-top: 38px;
+  color: ${({ theme, type }) =>
+    type === "total" ? theme.colors.shape : theme.colors.textDark};
+
+  font-size: ${RFValue(32)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
+
+export const LastTransaction = styled.Text<TypeProps>`
+  color: ${({ theme, type }) =>
+    type === "total" ? theme.colors.shape : theme.colors.text};
+
+  font-size: ${RFValue(12)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,10 +1,65 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container, Title } from "./styles";
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
+import {
+  Container,
+  Header,
+  UserInfo,
+  User,
+  UserGreeting,
+  UserName,
+  Photo,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from "./styles";
+
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/42949044?s=400&u=39e3d16ae6512aacedb920ac9adca57967a007e0&v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Iury</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.000,00"
+          lastTransaction="Útima entrada 05 de dezembro de 2021"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.500,00"
+          lastTransaction="Útima saída 05 de dezembro de 2021"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 15.500,00"
+          lastTransaction="01 à 30 de dezembro"
+          type="total"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   );
 };
