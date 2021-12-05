397113d975cca445bb26412f0c930d09e7652c04
Iury Sousa <iurysousa13@outlook.com>
2021-12-04 22:29:57 -0300

    feat: important concepts

--- myskills/src/pages/home.js
+++ myskills/src/pages/home.js
@@ -13,14 +13,28 @@ import { SkillCard } from '../components/SkillCard';
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greetting, setGreetting] = useState('');
 
   function handleNewAddNewSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreetting('Good morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreetting('Good afternoon');
+    } else {
+      setGreetting('Good night');
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Iury</Text>
+      <Text style={styles.greetings}>{greetting}</Text>
       <TextInput
         style={styles.input}
         placeholder="New skill"
@@ -60,5 +74,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#FFF',
   },
 });
