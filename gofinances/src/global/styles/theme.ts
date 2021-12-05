eb675cfbe23fdf960d0c6904160124a993faaf73
Iury Sousa <iurysousa13@outlook.com>
2021-12-05 10:54:49 -0300

    chore: add and configure syled-components

---
+++ gofinances/src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: "#5636D3",
+    secondary: "#FF872C",
+    secondaryLight: "rgba(255, 125, 44, .3)",
+
+    success: "#12A454",
+    successLight: "rgba(18, 164, 84, .5)",
+
+    attention: "#E83F5B",
+    attentionLight: "rgba(232, 63, 91, .5)",
+
+    shape: "#FFF",
+    title: "#363F5F",
+    text: "#969CB2",
+    background: "#F0F2F5",
+  },
+};
