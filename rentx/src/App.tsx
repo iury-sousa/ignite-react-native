03b661a2d1be8aeeb66a9e3688db26838cb0a5c7
Iury Sousa <iurysousa13@outlook.com>
2021-12-11 16:52:59 -0300

    chore: add inter and archivo fonts

---
+++ rentx/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Home } from "./screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    useFonts,
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <Home />;
+}
