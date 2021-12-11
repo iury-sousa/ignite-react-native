4ced2100274bbf6e96fa3d7907c49b6a7b79f26c
Iury Sousa <iurysousa13@outlook.com>
2021-12-11 18:58:13 -0300

    feat: creates header

--- rentx/src/screens/Home/styles.ts
+++ rentx/src/screens/Home/styles.ts
@@ -1,12 +1,30 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.backgroundPrimary};
   flex: 1;
-  justify-content: center;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 112px;
+  padding: 32px 16px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.secondary600};
+export const TotalCars = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary400};
 `;
