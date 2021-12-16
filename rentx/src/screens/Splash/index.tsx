2bd6e7911fd58f98ba6dd4f8352e852510cd3a57
Iury Sousa <iurysousa13@outlook.com>
2021-12-15 23:08:35 -0300

    feat: add splash

--- rentx/src/screens/Splash/index.tsx
+++ rentx/src/screens/Splash/index.tsx
@@ -1,48 +1,6 @@
 import React from "react";
-import { Button, Dimensions, StyleSheet } from "react-native";
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
-
 import { Container } from "./styles";
 
-const WIDTH = Dimensions.get("window").width;
-
 export function Splash() {
-  const animation = useSharedValue(0);
-  const animationStyles = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateX: withTiming(animation.value, {
-            duration: 500,
-            easing: Easing.ease,
-          }),
-        },
-      ],
-    };
-  });
-
-  function handleAnimationPosition() {
-    animation.value = Math.random() * (WIDTH - 100);
-  }
-
-  return (
-    <Container>
-      <Animated.View style={[styles.box, animationStyles]} />
-      <Button title="Mover" onPress={handleAnimationPosition} />
-    </Container>
-  );
+  return <Container></Container>;
 }
-
-const styles = StyleSheet.create({
-  box: {
-    width: 100,
-    height: 100,
-    margin: 10,
-    backgroundColor: "red",
-  },
-});
