80fc012ba58ffa6c059824452c77f7c8f765f2f2
Iury Sousa <iurysousa13@outlook.com>
2021-12-18 00:41:47 -0300

    feat: add icons

--- rentx/src/routes/app.stack.routes.tsx
+++ rentx/src/routes/app.stack.routes.tsx
@@ -7,10 +7,6 @@ import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { Confirmation } from "../screens/Confirmation";
 import { MyCars } from "../screens/MyCars";
-import { Splash } from "../screens/Splash";
-import { SignIn } from "../screens/SignIn";
-import { SignUpFirstStep } from "../screens/SignUp/FirstStep";
-import { SignUpSecondStep } from "../screens/SignUp/SecondStep";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -18,13 +14,7 @@ export function AppStackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
       {/* <Screen name="Splash" component={Splash} /> */}
-      <Screen
-        name="Home"
-        component={Home}
-        options={{
-          gestureEnabled: false,
-        }}
-      />
+      <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CardDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
