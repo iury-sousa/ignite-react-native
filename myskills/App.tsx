7af2b487e2ae65dceb77f839b558fc26dfc28b0b
Iury Sousa <iurysousa13@outlook.com>
2022-01-02 15:25:48 -0300

    feat: add android splash screen

--- myskills/App.tsx
+++ myskills/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/home';
+import SplashScreen from 'react-native-splash-screen';
 
 export function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
